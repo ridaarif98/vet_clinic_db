4284627874e342cba91ae12ecbe25c1b2d34629a
Rida Arif <ridaarif16@gmail.com>
2021-10-25 19:24:18 +0200

    Create Database and animal table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,17 @@
 /* Database schema to keep the structure of entire database. */
+/* Create Database*/
+CREATE DATABASE vet_clinic;
+
+
+
 
 CREATE TABLE animals (
-    name varchar(100)
+    name varchar(100),
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar(200),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutuered BOOLEAN,
+    weight_kg FLOAT,
+    PRIMARY KEY(id)
 );
