b03d6786d1af3768f44a8bc57a20c2ce7d8068ce
Rida Arif <ridaarif16@gmail.com>
2021-10-28 11:06:30 +0200

    Create vets table and join table for vet and species

--- schema.sql
+++ schema.sql
@@ -45,3 +45,23 @@ ADD CONSTRAINT constraint_fk FOREIGN KEY (species_id) REFERENCES species (id);
 -- Add column owner_id which is a foreign key referencing the owners table
 ALTER TABLE animals ADD COLUMN owners_id INT,
 ADD CONSTRAINT constraint_fk FOREIGN KEY (owners_id) REFERENCES owners (id);
+
+
+
+-- Create a table named vets
+CREATE TABLE vets(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCAHR(150),
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY(id)
+);
+
+-- Create a "join table" called specializations to handle relationship between the tables species and vets
+CREATE TABLE specializations(
+    vet_id INT,
+    species_id INT,
+    FOREIGN KEY(vet_id) REFERENCES vets(id),
+    FOREIGN KEY(species_id) REFERENCES species(id),
+    PRIMARY KEY(vet_id, species_id)
+);
